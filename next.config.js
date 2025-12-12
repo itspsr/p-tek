27f6d9c13aead5d8f757de76a05c18bc1a7a1057
pratik <pratik6472@gmail.com>
2025-12-12 20:30:46 +0530

    Fix: RSS thumbnail domains added

--- next.config.js
+++ next.config.js
@@ -4,18 +4,40 @@ const nextConfig = {
 
   images: {
     remotePatterns: [
+      // NYTimes
       { protocol: "https", hostname: "static01.nyt.com" },
-      { protocol: "https", hostname: "media.guim.co.uk" },
-      { protocol: "https", hostname: "*.bbci.co.uk" },
-      { protocol: "https", hostname: "*.bbc.co.uk" },
+      { protocol: "https", hostname: "www.nytimes.com" },
+
+      // BBC
+      { protocol: "https", hostname: "ichef.bbci.co.uk" },
+      { protocol: "https", hostname: "news.bbcimg.co.uk" },
+      { protocol: "https", hostname: "bbc.co.uk" },
+      { protocol: "https", hostname: "bbci.co.uk" },
+
+      // Guardian
       { protocol: "https", hostname: "i.guim.co.uk" },
-      { protocol: "https", hostname: "cdn.arstechnica.net" },
+      { protocol: "https", hostname: "media.guim.co.uk" },
+
+      // Verge
       { protocol: "https", hostname: "cdn.vox-cdn.com" },
+
+      // Wired
       { protocol: "https", hostname: "media.wired.com" },
+      { protocol: "https", hostname: "www.wired.com" },
+
+      // ArsTechnica
+      { protocol: "https", hostname: "cdn.arstechnica.net" },
+
+      // MarketWatch
+      { protocol: "https", hostname: "images.mktw.net" },
+      { protocol: "https", hostname: "www.marketwatch.com" },
+
+      // Investing.com
+      { protocol: "https", hostname: "images.investing.com" },
+      { protocol: "https", hostname: "www.investing.com" },
+
+      // Fallback (Unsplash)
       { protocol: "https", hostname: "images.unsplash.com" },
-      { protocol: "https", hostname: "*.marketwatch.com" },
-      { protocol: "https", hostname: "*.investing.com" },
-      { protocol: "https", hostname: "*" }, // universal fallback (useful)
     ],
   },
 
